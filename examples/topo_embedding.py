007287e24ac29bfc92147da2c4f5e1ed2294c1fd
Jose Pinilla <jpinilla@ece.ubc.ca>
2019-12-19 13:56:50 -0800

    Added runtime to example.

--- examples/topo_embedding.py
+++ examples/topo_embedding.py
@@ -17,27 +17,32 @@ C = dnx.chimera_graph(16, 16, coordinates=True)
 target_layout = {(i,j,u,k):(i,j) for (i,j,u,k) in C}
 dnx.draw_chimera(C, node_size=1)
 
+import time
 # For Quality Key metrics
 miner = minorminer.miner(G,C)
 
 # Minorminer
 plt.subplot(2, 2, 3)
+start = time.time()
 embedding = minorminer.find_embedding(G, C)
+end = time.time()
 dnx.draw_chimera_embedding(C, embedding, G, node_size=1)
 
 state, O, L = miner.quality_key(embedding,embedded=True)
 total = sum([L[i]*L[i+1] for i in range(0,len(L),2)])
-plt.title(f"Total: {total} Max: {L[0]}\n")
+plt.title(f"Total: {total} Max: {L[0]}\n t: {end - start:.2f}s")
 
 # Topominer
 # TEMP: Prototype version of topominer maps locations directly. Sloc->Tloc.
 plt.subplot(2, 2, 4)
+start = time.time()
 embedding = minorminer.topo_embedding(G, C, source_layout, target_layout)
+end = time.time()
 dnx.draw_chimera_embedding(C, embedding, G, node_size=1)
 
 state, O, L = miner.quality_key(embedding,embedded=True)
 total = sum([L[i]*L[i+1] for i in range(0,len(L),2)])
-plt.title(f"Total: {total} Max: {L[0]}\n")
+plt.title(f"Total: {total} Max: {L[0]}\n t: {end - start:.2f}s")
 
 plt.tight_layout()
 plt.show()
